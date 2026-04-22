553f54389e1acd91911fffe4e6d88024ad5058ed
PilarLF <pilofer1@uoc.edu>
2026-04-22 15:45:24 +0200

    a

--- frontend/src/app/auth/auth.service.ts
+++ frontend/src/app/auth/auth.service.ts
@@ -2,7 +2,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { BehaviorSubject } from 'rxjs';
-import { environment } from '../../environments/environment';
+import { environment } from '../../environments/enviroment.prod';
 
 export type Role = 'admin' | 'employee';
 
