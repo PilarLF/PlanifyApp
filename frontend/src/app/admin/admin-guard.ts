ee4b1ae77ee0b081376d2f5daddac559f2959feb
PilarLF <pilofer1@uoc.edu>
2026-05-21 17:19:55 +0200

    prueba empleados crear

--- frontend/src/app/admin/admin-guard.ts
+++ frontend/src/app/admin/admin-guard.ts
@@ -8,8 +8,8 @@ export const adminGuard: CanActivateFn = (route, state) => {
   const router = inject(Router);
   const auth = inject(AuthService);
 
-  const role = auth.getRole(); // debe devolver 'admin' | 'employee' | null
-  if (role === 'admin') return true;
+  const role = auth.getRole()?.trim().toUpperCase(); // debe devolver 'admin' | 'employee' | null
+  if (role === 'ADMIN') return true;
 
   // Si no es admin, redirige al panel de empleado o a forbidden
   console.log('Acceso denegado: se requiere rol de admin');
