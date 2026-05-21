0f1472c093f58b49c5be9e8e0aefe9c61eb411bf
PilarLF <pilofer1@uoc.edu>
2026-05-21 18:08:41 +0200

    socorro

--- frontend/src/app/auth/auth.service.ts
+++ frontend/src/app/auth/auth.service.ts
@@ -64,7 +64,7 @@ setToken(token: string) {
 
 
   register(data: FormData) {
-    return this.http.post(`${environment.apiUrl}/create-user`, data);
+    return this.http.post(`${environment.apiUrl}/auth/register`, data);
   }
 
 private loadFromStorage() {
