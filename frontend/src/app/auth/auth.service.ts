793989e110367c9b39d8589550f4a0e494ca8add
PilarLF <pilofer1@uoc.edu>
2026-05-21 17:28:07 +0200

    prueba empleados crear jope

--- frontend/src/app/auth/auth.service.ts
+++ frontend/src/app/auth/auth.service.ts
@@ -64,7 +64,7 @@ setToken(token: string) {
 
 
   register(data: FormData) {
-    return this.http.post(`${environment.apiUrl}/admin/create-user`, data);
+    return this.http.post(`${environment.apiUrl}/auth/admin/create-user`, data);
   }
 
 private loadFromStorage() {
